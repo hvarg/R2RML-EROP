d029da4a1939bd7c86d21530b95fb63aba9478a6
Hernán Vargas <hern.var@gmail.com>
2016-09-28 10:29:20 -0300

    fix function property type

--- function/function.sh
+++ function/function.sh
@@ -5,7 +5,7 @@ grep -Ev 'unknown|unidentified|resource:>|""\^\^' function.nt | sort -u > result
 mv result.nt function.nt
 cat <<EOT >> function.nt
 <http://bio2rdf.org/erop_vocabulary:function_class> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/1999/02/22-rdf-syntax-ns#Property> .
-<http://bio2rdf.org/erop_vocabulary:function_class> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#DatatypeProperty> .
+<http://bio2rdf.org/erop_vocabulary:function_class> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#ObjectProperty> .
 <http://bio2rdf.org/erop_vocabulary:function_class> <http://www.w3.org/2000/01/rdf-schema#label> "Function Class"^^<http://www.w3.org/2001/XMLSchema#string> .
 <http://bio2rdf.org/erop_vocabulary:function_class> <http://purl.org/dc/terms/description> "First function determination class"^^<http://www.w3.org/2001/XMLSchema#string> .
 EOT
